3bfa55d978dceca530041d647eb96a4a481dcad1
Karma Soliman <karma.soliman@epita.fr>
2025-11-04 15:28:46 +0100

    first proper deployment, all works!

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("test")
+import express from "express"
+
+const app = express()
+
+const PORT = 3000
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json({
+        first: "john",
+        last: "doe"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`ServerPort is running http://localhost:${PORT}`)
+})
